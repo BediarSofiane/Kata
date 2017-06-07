43b5baa480e3a40f9cc2db7378f63794e87fe629
Sofiane <bediar.sofiane@gmail.com>
2017-06-07 13:09:00 +0200

    configure port proxy (webPack) for Apis

--- webpack/webpack.dev.js
+++ webpack/webpack.dev.js
@@ -21,6 +21,8 @@ module.exports = webpackMerge(commonConfig({ env: ENV }), {
         proxy: [{
             context: [
                 /* jhipster-needle-add-entity-to-webpack - JHipster will add entity api paths here */
+                '/operation-api',
+                '/bank-account-api',
                 '/api',
                 '/management',
                 '/swagger-resources',
